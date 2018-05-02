f4a6e46c8f807520bc7d898001fc99f03c1631f5
acdibble <acdibble@gmail.com>
2018-05-02 18:50:37 -0500

    Add footer

---
+++ src/client/components/Bottom.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+const Bottom = () => (
+  <Navbar className="navbar-fixed-bottom">
+    <Nav>
+      <NavItem>
+        Thanks for reading!
+      </NavItem>
+    </Nav>
+  </Navbar>
+);
+
+export default Bottom;
