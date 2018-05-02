d09d25376799176db2353885b4a83e9c2905175f
acdibble <acdibble@gmail.com>
2018-05-02 17:36:00 -0500

    Implement modal to display article

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, Modal, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 class Main extends Component {
@@ -8,7 +8,10 @@ class Main extends Component {
 
     this.state = {
       articles: null,
+      id: '',
     };
+
+    this.handleToggle = this.handleToggle.bind(this);
   }
 
   async componentWillMount() {
@@ -16,26 +19,49 @@ class Main extends Component {
     this.setState({ articles: data });
   }
 
+  handleToggle(e) {
+    this.setState({
+      show: !this.state.show,
+      id: !e ? this.state.id : e.target.id,
+    });
+  }
+
   render() {
     if (!this.state.articles) return (<div>Hello world.</div>);
 
     const { articles } = this.state;
+
     return (
       <div className="container">
         <ListGroup>
           {Object.keys(articles).map((_id) => {
             const a = articles[_id];
             return (
-              <ListGroupItem
-                key={_id}
-                header={a.title}
+              <a
                 href="#"
+                className="list-group-item"
+                key={_id}
+                id={_id}
+                onClick={this.handleToggle}
               >
-                by {a.author}
-              </ListGroupItem>
+                <h4 id={_id} className="list-group-item-heading">{a.title}</h4>
+                <p id={_id} className="list-group-item-text">by {a.author}</p>
+              </a>
             );
           })}
         </ListGroup>
+
+        <Modal show={this.state.show} onHide={this.handleToggle}>
+          <Modal.Header closeButton>
+            <Modal.Title>{articles[this.state.id] ? articles[this.state.id].title : 'null'}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {articles[this.state.id] ? articles[this.state.id].body : 'null'}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleToggle}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
