2369dd0a9cc95fdd59a2f5c57fe2ed8cff165081
acdibble <acdibble@gmail.com>
2018-05-02 19:38:14 -0500

    Implement adding of articles through modal

--- src/client/components/Top.jsx
+++ src/client/components/Top.jsx
@@ -10,7 +10,9 @@ import {
   ControlLabel,
   FormControl,
   Col,
+  HelpBlock,
 } from 'react-bootstrap';
+import axios from 'axios';
 
 const { Header, Brand } = Navbar;
 
@@ -20,8 +22,14 @@ class Top extends Component {
 
     this.state = {
       show: false,
+      isDirty: false,
+      title: '',
+      author: '',
+      body: '',
+      tags: '',
     };
 
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
   }
@@ -32,8 +40,26 @@ class Top extends Component {
     });
   }
 
-  handleSubmit() {
-    console.log('hi');
+  async handleSubmit() {
+    const { author, body, title, tags } = this.state;
+    if (author && body && title) {
+      await axios.post('/api/articles', { author, body, title, tags: tags.replace(/\s/g, '') });
+      this.setState({
+        isDirty: false,
+        author: '',
+        body: '',
+        title: '',
+        tags: '',
+      });
+    } else {
+      this.setState({ isDirty: true });
+    }
+  }
+
+  handleChange({ target: { value, id } }) {
+    this.setState({
+      [id]: value,
+    });
   }
 
   render() {
@@ -58,39 +84,69 @@ class Top extends Component {
           </Modal.Header>
           <Modal.Body>
             <Form horizontal>
-              <FormGroup controlId="formHorizontalEmail">
+              <FormGroup validationState={!this.state.title && this.state.isDirty ? 'error' : null}>
                 <Col componentClass={ControlLabel} sm={2}>
                   Title
                 </Col>
                 <Col sm={10}>
-                  <FormControl type="text" placeholder="Title" />
+                  <FormControl
+                    onChange={this.handleChange}
+                    value={this.state.title}
+                    id="title"
+                    type="text"
+                    placeholder="Title"
+                  />
+                  <FormControl.Feedback />
+                  {!this.state.title && this.state.isDirty && <HelpBlock>Required</HelpBlock>}
                 </Col>
               </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
+              <FormGroup validationState={!this.state.author && this.state.isDirty ? 'error' : null}>
                 <Col componentClass={ControlLabel} sm={2}>
                   Author
                 </Col>
                 <Col sm={10}>
-                  <FormControl type="text" placeholder="Author" />
+                  <FormControl
+                    onChange={this.handleChange}
+                    value={this.state.author}
+                    id="author"
+                    type="text"
+                    placeholder="Author"
+                  />
+                  <FormControl.Feedback />
+                  {!this.state.author && this.state.isDirty && <HelpBlock>Required</HelpBlock>}
                 </Col>
               </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
+              <FormGroup validationState={!this.state.body && this.state.isDirty ? 'error' : null}>
                 <Col componentClass={ControlLabel} sm={2}>
                   Body
                 </Col>
                 <Col sm={10}>
-                  <FormControl componentClass="textarea" placeholder="Body" />
+                  <FormControl
+                    onChange={this.handleChange}
+                    value={this.state.body}
+                    id="body"
+                    componentClass="textarea"
+                    placeholder="Body"
+                  />
+                  <FormControl.Feedback />
+                  {!this.state.body && this.state.isDirty && <HelpBlock>Required</HelpBlock>}
                 </Col>
               </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
+              <FormGroup>
                 <Col componentClass={ControlLabel} sm={2}>
                   Tags
                 </Col>
                 <Col sm={10}>
-                  <FormControl type="text" placeholder={'Please separate tags only with commas, e.g. "news,austin,tech"'} />
+                  <FormControl
+                    onChange={this.handleChange}
+                    value={this.state.tags}
+                    id="tags"
+                    type="text"
+                    placeholder={'Please separate tags only with commas, e.g. "news,austin,tech"'}
+                  />
                 </Col>
               </FormGroup>
             </Form>
