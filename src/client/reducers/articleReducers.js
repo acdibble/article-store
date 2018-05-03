eeb0456c7043792b51f1ed5c669ba1f7f758ead2
acdibble <acdibble@gmail.com>
2018-05-02 21:16:00 -0500

    Add reducer for new deletion action with console log

--- src/client/reducers/articleReducers.js
+++ src/client/reducers/articleReducers.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL_ARTICLES, CREATE_AND_ADD_TO_STATE } from '../actions/articleActions';
+import { FETCH_ALL_ARTICLES, CREATE_AND_ADD_TO_STATE, DELETE_AND_REMOVE_FROM_STATE } from '../actions/articleActions';
 
 const articles = (state = {}, { type, payload }) => {
   switch (type) {
@@ -6,6 +6,9 @@ const articles = (state = {}, { type, payload }) => {
       return { ...payload };
     case CREATE_AND_ADD_TO_STATE:
       return { ...state, ...payload };
+    case DELETE_AND_REMOVE_FROM_STATE:
+      console.log(payload)
+      return state;
     default:
       return state;
   }
